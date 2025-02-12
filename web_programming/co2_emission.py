546953b504b49e8f6116b859e81c255b160ac629
lokeshwaranbm <lokeshwaranbm6@gmail.com>
2025-02-12 09:43:34 +0530

    hello

--- web_programming/co2_emission.py
+++ web_programming/co2_emission.py
@@ -9,7 +9,6 @@ import requests
 BASE_URL = "https://api.carbonintensity.org.uk/intensity"
 
 
-# Emission in the last half hour
 def fetch_last_half_hour() -> str:
     last_half_hour = requests.get(BASE_URL, timeout=10).json()["data"][0]
     return last_half_hour["intensity"]["actual"]
